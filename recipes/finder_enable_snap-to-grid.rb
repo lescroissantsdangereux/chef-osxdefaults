92e9ba277b5bc1c78a6108c08ee6b94ab435573a
lescroissantsdangereux <james@Jamess-MacBook-Pro.local>
2014-04-25 00:45:16 +0100

    1 more recipe
    recipe to enable snap-to-grid in finder

---
+++ recipes/finder_enable_snap-to-grid.rb
@@ -0,0 +1,11 @@
+execute "Enable snap-to-grid for icons on the desktop" do
+  command "/usr/libexec/PlistBuddy -c \"Set :DesktopViewSettings:IconViewSettings:arrangeBy grid\" ~/Library/Preferences/com.apple.finder.plist"
+end
+
+execute "Enable snap-to-grid in other icon views 1/2" do
+  command "/usr/libexec/PlistBuddy -c \"Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid\" ~/Library/Preferences/com.apple.finder.plist"
+end
+
+execute "Enable snap-to-grid in other icon views 2/2" do
+  command "/usr/libexec/PlistBuddy -c \"Set :StandardViewSettings:IconViewSettings:arrangeBy grid\" ~/Library/Preferences/com.apple.finder.plist"
+end
