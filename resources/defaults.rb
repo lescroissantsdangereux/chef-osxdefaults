4e764ce5ff1a118ce1ac33f8ab99ab9dc0dd3611
lescroissantsdangereux <james@Jamess-MacBook-Pro.local>
2014-04-25 01:41:34 +0100

    enable array support
    take 2

--- resources/defaults.rb
+++ resources/defaults.rb
@@ -36,7 +36,7 @@ attribute :integer, :kind_of => Integer, :default => nil
 attribute :string, :kind_of => String, :default => nil
 attribute :boolean, :kind_of => [ TrueClass, FalseClass ], :default => nil
 attribute :float, :kind_of => [Float, Integer], :default => nil
-attribute :array, :kind_of => [Array, String, Integer], :defaults => nil
+attribute :array, :kind_of => [Array, String, Integer], :default => nil
 
 def initialize(name, run_context=nil)
   super
